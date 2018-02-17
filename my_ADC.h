1e2e7b8c08e63c303822f67936e4e8dd92514b4c
karimsharabash <36041174+karimsharabash@users.noreply.github.com>
2018-02-17 18:26:34 +0200

    Update my_ADC.h

--- my_ADC.h
+++ my_ADC.h
@@ -1,14 +1,14 @@
 /*
  * my_ADC.h
  *
- *  Created on: ??þ/??þ/????
- *      Author: ascom
+ *  Created on: 15/2/2018
+ *      Author: karim sharabash
  */
 
 #ifndef MY_ADC_H_
 #define MY_ADC_H_
 
 void ADC_Init();
-unsigned int ADC_Read(unsigned char channel,float Vref );
+unsigned int ADC_Read(unsigned char channel);
 
 #endif /* MY_ADC_H_ */
