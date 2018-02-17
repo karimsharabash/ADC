c75b323574c681d7892625fb5f3b13382edaa742
karimsharabash <36041174+karimsharabash@users.noreply.github.com>
2018-02-17 18:25:47 +0200

    Update my_ADC.c

--- my_ADC.c
+++ my_ADC.c
@@ -1,33 +1,27 @@
 /*
  * my_ADC.c
  *
- *  Created on: ??þ/??þ/????
- *      Author: ascom
+ *  Created on: 15/2/2018
+ *     Author: karim sharabash
  */
 #include<avr/io.h>
 
 void ADC_Init()
 {
-
-	ADCSRA=(1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);						 // 1- Enable ADC 2- Define Prescaler 128
+	ADMUX=(1<<REFS0);                                             // choose the Vref
+	ADCSRA=(1<<ADEN)|(1<<ADPS1)|(1<<ADPS2);	         	  //  Enable ADC ana set Prescaler /64
 
 }
-unsigned int ADC_Read(unsigned char channel,float Vref )
+unsigned int ADC_Read(unsigned char channel)
 {
 
-	ADMUX=channel;		        // 3- select ADC channel
-	if(Vref==2.56f)              // 4- Select vref
-	{
-		ADMUX|=(1<<REFS0)|(1<<REFS1);       // AVCC=2.56v
-	}
-	else{
-		ADMUX|=(1<<REFS0);        // AVCC=5VA  (default if u didn't choose 2.56 v)
-	}
-
-	ADCSRA|=(1<<ADSC);          // 5- Start conversion for ADC
-	while(!(ADCSRA&(1<<ADIF))); // 6- Wait till conversion complete
-	ADCSRA|=(1<<ADIF);			// 7- Clear ADIF
-	return ADC;					// 8- return ADC
+	channel &=0x07;                                              //   remove the recent channel
+	ADMUX |=channel;                                             //   select ADC channel
+	ADCSRA|=(1<<ADSC);                                           //  Start conversion for ADC
+	while(!(ADCSRA&(1<<ADIF)));                                  //  Wait till conversion complete
+	ADCSRA|=(1<<ADIF);		                            //  Clear ADIF
+//	while(ADCSRA & (1<<ADSC));                                   //  Wait till conversion complete
+	return ADC;				                     //  return digital equivalent to the anaglog reading
 
 }
 
